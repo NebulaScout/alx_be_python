d7af78226329bd288042a00b7863d18b91450c37
NebulaScout <ribak427@gmail.com>
2025-09-14 09:59:17 +0300

    Perform basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,18 @@
+# Basic Arithmetic operations
+
+number1 = 10
+number2 = 5
+
+# Addition
+result = number1 + number2
+
+print("Addition of", number1, "and", number2, "is:", result)
+
+# Subtraction
+result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is:", result)
+
+# Multiplication
+result = number1 * number2
+print("Multiplication of", number1, "and", number2, "is:", result)
+
