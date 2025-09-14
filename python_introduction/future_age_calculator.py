ecd61cec5ef78394b1002cff306645608b437dbf
NebulaScout <ribak427@gmail.com>
2025-09-14 10:46:33 +0300

    Create an age calculator that displays your expected age in the future years

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# Age Calculator
+
+current_age = int(input("How old are you?"))
+current_year = 2023
+future_year = 2050
+years_to_add = future_year - current_year
+
+age = current_age + years_to_add
+
+print("In 2050, you will be", age,"years old.")
