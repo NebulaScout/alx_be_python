24ed0a53e87ad3a119325a94bffdf1b3a0abd7c7
NebulaScout <ribak427@gmail.com>
2025-09-28 23:46:31 +0300

    create a module performs specified operations with dates and times

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    return current_date.strftime("%Y-%m-%d %H:%M:%S")
+
+def calculate_future_date(number_of_days):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=number_of_days)
+    return future_date.strftime("%Y-%m-%d")
+
+print("Current Date and Time:", display_current_datetime())
+days = int(input("Enter number of days to add: "))
+future_date = calculate_future_date(days)
+print(f"Future Date: {future_date}")
